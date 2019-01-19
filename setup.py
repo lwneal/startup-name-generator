1be80a58e9a199c3b21fc683db855d1ac38841c4
Lawrence Neal <nealla@lwneal.com>
2019-01-18 18:10:37 -0800

    Add data files for bdist. Or is it sdist?
    A ship called Titanic. A zeppelin called Hindenburg.
    A library packaging tool called Distutil.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+setup(name='startup_name_generator',
+    version='1.0.1',
+    description='Generate a domain name for your new startup.',
+    author='Larry Neal',
+    author_email='nealla@lwneal.com',
+    packages=[
+        'startup_name_generator',
+    ],
+    package_data={
+        'startup_name_generator': [
+            'startup_name_generator/*.txt',
+        ],
+    },
+    install_requires=[
+        "flask",
+    ],
+    scripts=[
+        'generate_startup_name',
+    ],
+    include_package_data=True,
+)
+
