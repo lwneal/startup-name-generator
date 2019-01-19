745863c5207c1a7c74d16d8a278058ecf5f4938e
Lawrence Neal <nealla@lwneal.com>
2019-01-18 22:55:13 -0800

    Python 2/3 compatability fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 
 
 setup(name='startup_name_generator',
-    version='1.0.1',
+    version='1.0.2',
     description='Generate a domain name for your new startup.',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
