a08c0a6d69002a062287c6f1501dc3ac8520f05d
Lawrence Neal <nealla@lwneal.com>
2020-03-28 11:28:24 -0700

    Add favicon

--- server.py
+++ server.py
@@ -10,6 +10,9 @@ def main_page():
     startup_name = name()
     return flask.render_template('index.html', startup_name=startup_name)
 
+@app.route('/favicon.ico')
+def favicon():
+    return flask.send_from_directory('static', 'favicon.ico')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8042)
