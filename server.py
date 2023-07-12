3d2f7c8e5e211854d56487ed2d3d4cbe1cd06e01
nealla@lwneal.com <nealla@lwneal.com>
2023-07-12 01:25:07 +0000

    Add proxyfix for X-Real-IP et al

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import flask
 from startup_name_generator.name_generator import name
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 app = flask.Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 
 @app.route('/')
@@ -15,4 +17,4 @@ def favicon():
     return flask.send_from_directory('static', 'favicon.ico')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8005, threaded=True)
+    app.run('127.0.0.1', port=8005, threaded=True)
